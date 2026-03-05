cfbf39d42f2cf04d9f4886db1197ca15657a250b
Marcelo Thiel <marcelo.thiel@escola.pr.gov.br>
2026-03-05 14:56:50 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const avanca = document.querySelectorAll('.btn-proximo');
 avanca.forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
-        // Agora ele vai buscar exatamente 'passo-1', 'passo-2', etc.
         const proximoId = 'passo-' + this.getAttribute('data-proximo');
 
-        atual.classList.remove('ativo');
-        document.getElementById(proximoId).classList.add('ativo');
+        if (atual) { // Verifica se encontrou o elemento ativo
+            atual.classList.remove('ativo');
+        }
+        
+        const proximoElemento = document.getElementById(proximoId);
+        if (proximoElemento) {
+            proximoElemento.classList.add('ativo');
+        }
     })
 })
